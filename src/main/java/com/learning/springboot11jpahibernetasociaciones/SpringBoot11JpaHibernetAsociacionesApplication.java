83331e8b2139821cf36b6ee3b5d1abb2f83d60f0
Moises Silva Cardenas <moises.silva@et.bo>
2024-08-01 11:38:44 -0400

    156. Codificando ejemplo ManyToOne

--- src/main/java/com/learning/springboot11jpahibernetasociaciones/SpringBoot11JpaHibernetAsociacionesApplication.java
+++ src/main/java/com/learning/springboot11jpahibernetasociaciones/SpringBoot11JpaHibernetAsociacionesApplication.java
@@ -1,13 +1,38 @@
 package com.learning.springboot11jpahibernetasociaciones;
 
+import com.learning.springboot11jpahibernetasociaciones.entities.Client;
+import com.learning.springboot11jpahibernetasociaciones.entities.Invoice;
+import com.learning.springboot11jpahibernetasociaciones.repositories.ClientRepository;
+import com.learning.springboot11jpahibernetasociaciones.repositories.InvoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot11JpaHibernetAsociacionesApplication {
+public class SpringBoot11JpaHibernetAsociacionesApplication implements CommandLineRunner {
+
+    @Autowired
+    private ClientRepository clientRepository;
+    @Autowired
+    private InvoiceRepository invoiceRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot11JpaHibernetAsociacionesApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        manyToOne();
+    }
+
+    public void manyToOne(){
+        Client client = Client.builder().name("Margarita").lastname("Rios").build();
+        clientRepository.save(client);
+
+        Invoice invoice = Invoice.builder().description("compras de oficina").total(145d).build();
+        invoice.setClient(client);
+        Invoice invoiceDb = invoiceRepository.save(invoice);
+        System.out.println(invoiceDb);
+    }
 }
